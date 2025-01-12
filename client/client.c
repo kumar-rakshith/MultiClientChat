f7d5b8f158681d7849f621d621dd901a923a8f38
Rakshith Kumar <rakshithkumar.thokur@gmail.com>
2025-01-13 05:01:22 +0530

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,1 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
+#define SERVER_IP "192.168.110.178"
+#define PORT 5000
+#define BUFFER_SIZE 1024
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // Create socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Set up server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    if (inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to server
+    if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server.\n");
+
+    // Communication loop
+    while (1) {
+        // Send message to server
+        printf("Client: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        // Receive response from server
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received <= 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+        printf("Server: %s\n", buffer);
+    }
+
+    close(client_socket);
+    return 0;
+}
